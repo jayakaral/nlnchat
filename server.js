fef2435e11afc096e5913cc0117553cfbfd41bed
jayakaral <85970889+jayakaral@users.noreply.github.com>
2021-10-30 19:02:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,35 @@ const app = express()
 const http = require('http').createServer(app)
 
 const PORT = process.env.PORT || 3000
-
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
-})
+});
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
-})
+});
 
 // Socket 
 const io = require('socket.io')(http)
 
-io.on('connection', (socket) => {
-    console.log('Connected...')
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg)
-    })
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        // console.log("new user", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
 
 })
