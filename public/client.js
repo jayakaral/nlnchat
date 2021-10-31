afc66c2c32fbd924cb49d58cbda4e7fb6aa8b38e
jayakaral <85970889+jayakaral@users.noreply.github.com>
2021-10-31 20:43:58 +0530

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,17 +1,18 @@
 const socket = io();
-
+let name;
 const sendbtn = document.getElementById('send-btn');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('.container');
 // var audio = new Audio('ting.mp3');
-const name = prompt("enter your name to join");
-
+do {
+    name = prompt('Please enter your name: ')
+} while (!name)
 
 const append = (cname, message, position) => {
     const d = new Date();
     let lt = d.toLocaleTimeString();
     const messageElement = document.createElement('div');
-    messageElement.innerHTML = `<h5>${cname} ${lt}</h5><p>${message}</p>`;
+    messageElement.innerHTML = `<h5>${cname} | ${lt}</h5><p>${message}</p>`;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
@@ -26,6 +27,9 @@ messageInput.oninput = () => {
     } else {
         sendbtn.style.display = "block";
     }
+    messageInput.style.height = 'auto';
+    messageInput.style.height = messageInput.scrollHeight + 'px';
+
 };
 
 sendbtn.addEventListener('click', () => {
@@ -34,6 +38,7 @@ sendbtn.addEventListener('click', () => {
     socket.emit('send', message);
     sendbtn.style.display = "none";
     messageInput.value = "";
+    messageInput.style.height = '43px';
     scrollToBottom();
 })
 
